7ab7ffecde0096193da988abae61af7984428e67
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-02-18 19:13:10 -0600

    Crear template de personajes

---
+++ src/templates/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    const view = `
+        <div class="Header-main">
+            <div class="Header-logo">
+            <h1><a href="/">100tifi.co</a></h1>
+            </div>
+        </div>
+        
+        <div class="Header-nav">
+        <a href="#/about/">About</a>
+        </div>
+    `;
+    return view;
+}
+
+export default Header;
