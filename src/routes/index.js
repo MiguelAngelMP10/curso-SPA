1774bcd8a946dd6937a7f7980010608dc15718aa
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-02-18 20:15:28 -0600

    Conectar las rutas con los templates

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import Header from '../templates/Header';
 import Home from '../pages/Home';
 import Character from '../pages/Character';
 import Error404 from '../pages/Error404';
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes';
 
 const routers = {
     '/': Home,
@@ -14,6 +16,8 @@ const router = async () => {
     const content = null || document.getElementById('content');
 
     header.innerHTML = await Header();
+
+
 
 };
 
